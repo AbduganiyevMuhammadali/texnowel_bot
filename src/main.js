eed8d2d18b49639bd43e93257c6b96b111d5f4ca
AbduganiyevMuhammadali <muhammadalijunior07@gmail.com>
2024-11-23 23:30:10 +0500

    ssh server

--- src/main.js
+++ src/main.js
@@ -7,8 +7,8 @@ import Axios from 'axios'
 import VueEasyLightbox from 'vue-easy-lightbox'
 import YmapPlugin from "vue-yandex-maps"
 
-let MIDDLEWARE_URL = 'https://middleware.bdm.uz/'
-let VITE_BASE_URL = 'https://aa0663-api.akhmad.uz/'
+// let MIDDLEWARE_URL = 'https://middleware.bdm.uz/'
+let VITE_BASE_URL = 'https://aa0663-api.akhmad.uz/api/v1/'
 Axios.defaults.baseURL = VITE_BASE_URL
 Axios.defaults.timeout = 20000
 
